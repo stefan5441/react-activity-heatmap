8ed3c2cd5ae56b8aeede8e813feb1937b0fc434f
stefan5441 <stefan.cambov2000@gmail.com>
2026-01-08 14:39:00 +0100

    [FTR] Updating demo, adding styling

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -1,11 +1,39 @@
-import { ActivityHeatmap } from "../../src/ActivityHeatmap";
+import { activities } from "./utils";
+import { FaCode, FaNpm } from "react-icons/fa";
+import { ActivityHeatmap } from "react-activity-heatmap";
 
 import styles from "./App.module.css";
 
 function App() {
   return (
     <div className={styles.container}>
-      <ActivityHeatmap activities={[]} />
+      <h1>React Activity Heatmap Demo</h1>
+
+      <div className={styles.activityHeatmapContainer}>
+        <ActivityHeatmap activities={activities} />
+      </div>
+
+      <p>Install using npm install react-activity-heatmap and enjoy. ❤️</p>
+
+      <div className={styles.footer}>
+        <a
+          href="https://github.com/stefan5441/react-activity-heatmap"
+          className={styles.link}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaCode size={20} /> github
+        </a>
+
+        <a
+          href="https://www.npmjs.com/package/react-activity-heatmap"
+          className={styles.link}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaNpm size={20} /> npm
+        </a>
+      </div>
     </div>
   );
 }
